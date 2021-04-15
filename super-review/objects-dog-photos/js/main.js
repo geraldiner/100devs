eef9cc1a5d51248e1befdbf955ef1a0019e43c9a
geraldiner <hello@geraldiner.com>
2021-04-15 08:51:33 -1000

    objects dog photos exercise

--- super-review/objects-dog-photos/js/main.js
+++ super-review/objects-dog-photos/js/main.js
@@ -1,1 +1,21 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+async function setDogPhoto() {
+  let dogPhoto = await fetchDogInfo();
+  const img = document.querySelector('img');
+  img.src = dogPhoto.message;
+}
+
+
+async function fetchDogInfo() {
+  const url = 'https://dog.ceo/api/breeds/image/random';
+  try {
+    let dogStream = await fetch(url);
+    let dogJson = await dogStream.json();
+    return dogJson;
+  } catch (err) {
+    return { Error: error.stack }
+  }
+}
+
+window.addEventListener('load', setDogPhoto)
