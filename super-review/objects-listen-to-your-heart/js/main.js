b3d10173a90390505937529ec5e29cb8e9e307f9
geraldiner <hello@geraldiner.com>
2021-04-15 09:57:13 -1000

    stopwatch object exercise

--- super-review/objects-listen-to-your-heart/js/main.js
+++ super-review/objects-listen-to-your-heart/js/main.js
@@ -3,3 +3,56 @@
 let stopwatch = {};
 
 stopwatch.color = 'black';
+stopwatch.buttons = ['start', 'stop', 'pause', 'reset'];
+stopwatch.type = 'phone';
+stopwatch.brand = 'Seiko';
+stopwatch.mode = 'stop';
+stopwatch.time = '00:00:00'
+
+stopwatch.start = () => {
+  stopwatch.mode = 'start';
+  alert("The stopwatch is starting!")
+}
+
+stopwatch.stop = () => {
+  stopwatch.mode = 'stop';
+  alert("The stopwatch has stopped.")
+}
+
+stopwatch.pause = () => {
+  stopwatch.mode = 'pause';
+  alert("The stopwatch is paused.")
+}
+
+stopwatch.reset = () => {
+  stopwatch.mode = 'reset';
+  alert("The stopwatch has been reset.")
+}
+
+stopwatch.getTime = () => {
+  return stopwatch.time;
+}
+
+stopwatch.setMode = (e) => {
+  let mode = e.target.attributes[1].value;
+  stopwatch.mode = mode;
+  switch (mode) {
+    case 'start':
+      stopwatch.start();
+      break;
+    case 'stop':
+      stopwatch.stop();
+      break;
+    case 'pause':
+      stopwatch.pause();
+      break;
+    case 'reset':
+      stopwatch.reset();
+      break;
+  }
+}
+
+let stopwatchBtns = document.querySelectorAll('button');
+stopwatchBtns.forEach((btn) => {
+  btn.addEventListener('click', stopwatch.setMode);
+})
