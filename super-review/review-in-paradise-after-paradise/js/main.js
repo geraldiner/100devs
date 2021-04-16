594a2a07b127d3ba9d6ab3a51958a37549fc1a6e
geraldiner <hello@geraldiner.com>
2021-04-16 09:59:55 -1000

    paradise after paradise exercise

--- super-review/review-in-paradise-after-paradise/js/main.js
+++ super-review/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,15 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function checkNumbers(arr) {
+  if (arr[0] < arr[1]) {
+    alert("Hi");
+  } else if (arr[0] > arr[1]) {
+    alert("Bye");
+  } else {
+    alert("We close in an hour");
+  }
+}
+
+checkNumbers([1, 2])
+checkNumbers([10, 2])
+checkNumbers([2, 2])
