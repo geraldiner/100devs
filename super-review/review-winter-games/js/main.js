168f6cf0fec32ef15858912c4484995dcf389391
geraldiner <hello@geraldiner.com>
2021-04-16 18:19:06 -1000

    winter games exercise

--- super-review/review-winter-games/js/main.js
+++ super-review/review-winter-games/js/main.js
@@ -1,1 +1,17 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+
+function filterEvenNumbers(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr;
+}
+
+let arr = [51, 4, 7, 2, 8, 10, 1];
+console.log(filterEvenNumbers(arr));
+
+let arr2 = [1, 24, 17, 32, 28, 101, 51];
+console.log(filterEvenNumbers(arr2));
