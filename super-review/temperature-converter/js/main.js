4d26c66614b777f1f8bedbae0b5229b96894bb7e
geraldiner <hello@geraldiner.com>
2021-04-16 19:25:32 -1000

    temperature converter

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
 //Write your pseduo code first! 
+let p = document.querySelector('#temperature');
+let btn = document.querySelector('#submitTemp');
+let input = document.querySelector('#inputTemp');
+let form = document.querySelector('form');
+
+btn.addEventListener('click', displayTemp);
+
+function displayTemp(e) {
+  e.preventDefault();
+  let celsius = parseInt((input.value - 32) * (5 / 9));
+  p.textContent = `${input.value}°F is ${celsius}°C.`;
+  form.reset();
+}
