b500fd1ee60c7973cbcb0978b35e9d410cadf081
geraldiner <hello@geraldiner.com>
2021-06-01 21:03:24 -1000

    working on understanding js methods

---
+++ methods/array_methods.js
@@ -0,0 +1,49 @@
+// map
+// MDN:
+// The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+// You use .map() to create a new array based on what you did on every element in the array it was called on
+// I know this one fairly well and confident in its use. Like if you wanted to turn an array of strings that were numbers into actual numbers
+// @params: bacllback function that is called on every element on the array that map was called on
+// @return: new array
+
+let arr1 = ["1", "2", "3", "4"] // array of numbers that are strings
+let arr2 = arr1.map((a) => Number(a)) // new array of the numbers as Numbers
+console.log(arr1, arr2)
+
+// Turn an array of numbers into an array of their squares
+let arr3 = [1, 3, 5, 7, 9] // array of numbers
+let arr4 = arr3.map(a => a ** 2)
+console.log(arr3, arr4)
+
+// reduce
+// MDN:
+// The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
+// This one is hard. Uses a reducer function (like called recursively?) on each element of the array to get one single output value - which can be more than just a number. It can also be an array, maybe a string?
+// @params: accumulator = the number keeping the resulting single output
+//          currentValue = the value the callback function is currently on
+
+// filter
+// forEach
+// sort
+// slice
+// pop
+// shift
+// push
+// unshift
+// includes
+// indexOf
+// every
+// Give a short description of what the method does, how it works, it's time complexity (if appropriate), and give three examples of it in action!
+// Questions to practice:
+// One:
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// Two:
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+// Three:
+// Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+// Example:
+// [ 6, 2, 1, 8, 10 ] => 16
+// [ 1, 1, 11, 2, 3 ] => 6
+// 5 codewars tagged as arrays (minimum)
+// Add what you learn to an interview deck on Anki!
