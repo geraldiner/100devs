ab343128c79f89b9d540e1a83d1a0b8157b67a0f
Geraldine <hello@geraldiner.com>
2022-02-20 16:46:36 -0500

    add: practice js problems from class12

--- coursework/class12/class12-materials/class-weekend-boring/js/main.js
+++ coursework/class12/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,16 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector("#check").addEventListener("click", check);
+const display = document.querySelector("#placeToSee");
+const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 function check() {
-
-  const day = document.querySelector('#day').value
-
-  //Conditionals go here
-
-
+	const day = document.querySelector("#day").value;
+	if (day === "" || !days.includes(day)) {
+		alert(`Please input a day of the week: ${days.join(", ")}`);
+	} else if (day.toLowerCase() === "tuesday" || day.toLowerCase() === "thursday") {
+		display.innerText = "IT'S TIME FOR CLASS";
+	} else if (day.toLowerCase() === "sunday") {
+		display.innerText = "IT'S TIME FOR CHILL OFFICE HOURS";
+	} else {
+		display.innerText = "THERE'S NO CLASS TODAY";
+	}
 }
